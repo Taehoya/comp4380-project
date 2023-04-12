96254c14b95f1d3526ef19a3cf3b2444fcd4feb2
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:38:28 -0500

    Update q11.sql

--- q11.sql
+++ q11.sql
@@ -1,1 +1,12 @@
 /* Query 11 */
+
+SELECT 
+  COUNT(1) AS COUNT, SUM([Tips])-SUM([Trip Total]-[Tips]) AS Amount, Company AS Company 
+FROM
+  dbo.taxiTrips
+WHERE 
+  [Fare] < [Tips]
+GROUP BY 
+  Company
+ORDER BY 
+  COUNT DESC;
