0c157aa33d35afb7b76011e42ea2a85d388d7c5c
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:46:03 -0500

    Update q13.sql

--- q13.sql
+++ q13.sql
@@ -1,5 +1,6 @@
 /* Query 13 */
 
+/* Original Query */
 SELECT * 
 FROM
   dbo.taxiTrips
@@ -15,3 +16,34 @@ OR
     AND 
       [Trip Miles] = 0
   );
+
+/* Optimized Query */
+SELECT * 
+FROM
+  dbo.taxiTrips
+WHERE
+  (
+      Fare = 0 
+  OR 
+      [Trip Total] = 0 
+  ) 
+AND
+  (
+      Fare = 0 
+    OR
+      [Trip Miles] = 0
+  ) 
+AND 
+  ( 
+      Tips = 0 
+   OR 
+      [Trip Total] = 0 
+  ) 
+AND 
+  (
+      Tips = 0 
+    OR
+      [Trip Miles] = 0
+  );
+  
+  
