f3d7f6e437b441ce125fc0fc842388e7230836ea
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:29:12 -0500

    Update q3.sql

--- q3.sql
+++ q3.sql
@@ -1,5 +1,6 @@
 /* Query 3 */
 
+/* Original Query */
 SELECT * 
 FROM 
   dbo.taxiTrips 
@@ -7,3 +8,14 @@ WHERE
   [Trip Start Timestamp] >= '2019-01-01 00:00:00.000' 
 AND 
   [Trip End Timestamp] < '2019-12-31 23:59:59.999';
+  
+ /* Optimized Query */
+SELECT 
+  [Trip ID], [Taxi ID] 
+FROM 
+  dbo.taxiTrips 
+WHERE 
+  [Trip Start Timestamp] >= '2019-01-01 00:00:00.000' 
+AND
+  [Trip End Timestamp] < '2019-12-31 23:59:59.999';
+
