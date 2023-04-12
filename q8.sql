5264847502dcac79902ff2b56de1985e3ed4af6c
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:23:20 -0500

    Update q8.sql

--- q8.sql
+++ q8.sql
@@ -1,7 +1,12 @@
 /* Query 8 */
 
-SELECT [Pickup Centroid Latitude], [Pickup Centroid Longitude], SUM(Tips)/count(1) AS average
-FROM dbo.taxiTrips
-WHERE [Pickup Centroid Latitude] != ''
-GROUP BY [Pickup Centroid Latitude], [Pickup Centroid Longitude]
-ORDER BY average desc;
+SELECT 
+  [Pickup Centroid Latitude], [Pickup Centroid Longitude], SUM(Tips)/count(1) AS average
+FROM 
+  dbo.taxiTrips
+WHERE 
+  [Pickup Centroid Latitude] != ''
+GROUP BY 
+  [Pickup Centroid Latitude], [Pickup Centroid Longitude]
+ORDER BY 
+  average desc;
