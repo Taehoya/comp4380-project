3cc7ead26afd5fe0b0dbbcc22e35533a8142f9c8
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:17:26 -0500

    Update q9.sql

--- q9.sql
+++ q9.sql
@@ -2,8 +2,13 @@
 
 SELECT * FROM dbo.taxiTrips                                      
 WHERE [Company] = 'City Service' 
-AND [Payment Type] = 'Mobile' 
-AND [Trip Seconds] > 1800) 
-OR [Company] = 'Sun Taxi' 
-AND [Payment Type] = 'Mobile' 
-AND [Trip Seconds] > 1800);
+AND 
+[Payment Type] = 'Mobile' 
+AND 
+[Trip Seconds] > 1800) 
+OR 
+[Company] = 'Sun Taxi' 
+AND 
+[Payment Type] = 'Mobile' 
+AND 
+[Trip Seconds] > 1800);
