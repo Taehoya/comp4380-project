455d8576ef9aa766ac3e61b10ffecbdc01795045
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:13:44 -0500

    Create q9.sql

---
+++ q9.sql
@@ -0,0 +1,9 @@
+/* Query 9 */
+
+SELECT * FROM dbo.taxiTrips                                      
+WHERE [Company] = 'City Service' 
+AND [Payment Type] = 'Mobile' 
+AND [Trip Seconds] > 1800) 
+OR [Company] = 'Sun Taxi' 
+AND [Payment Type] = 'Mobile' 
+AND [Trip Seconds] > 1800);
