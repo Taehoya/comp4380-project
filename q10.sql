de6ba2c7361197a6688913a3e4ed8885102bf1a5
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:21:49 -0500

    Update q10.sql

--- q10.sql
+++ q10.sql
@@ -2,17 +2,24 @@
 
 /* Original Query */
 SELECT *
-FROM dbo.TaxiTrips
-WHERE [Trip Seconds] > 3600
+FROM 
+  dbo.TaxiTrips
+WHERE 
+  [Trip Seconds] > 3600
 AND
-[Trip Miles] > 6.214
-ORDER BY [Trip Seconds] ASC;
+  [Trip Miles] > 6.214
+ORDER BY 
+  [Trip Seconds] ASC;
 
 /* Optimized Query */
-SELECT [Trip ID], [Trip Seconds], [Trip Miles] 
-FROM dbo.taxiTrips 
-WHERE [Trip Seconds] > 3600 
+SELECT 
+  [Trip ID], [Trip Seconds], [Trip Miles] 
+FROM 
+  dbo.taxiTrips 
+WHERE 
+  [Trip Seconds] > 3600 
 AND
-[Trip Miles] > 6.214 
-ORDER BY [Trip Seconds] ASC;
+  [Trip Miles] > 6.214 
+ORDER BY 
+  [Trip Seconds] ASC;
 
