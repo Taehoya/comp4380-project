aab330916430246a2701b91cbebb76e3e14ff94b
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:28:10 -0500

    Update q2.sql

--- q2.sql
+++ q2.sql
@@ -1,7 +1,22 @@
 /* Query 2 */
 
+/* Original Query */
 SELECT * 
 FROM
   dbo.taxiTrips 
 WHERE
   [Payment Type] = 'Cash'; 
+
+/* Optimized Query */
+SELECT 
+  [Trip ID], [Taxi ID] 
+FROM 
+  dbo.taxiTrips
+WHERE [Trip ID]
+IN (SELECT 
+      [Trip ID] 
+    FROM 
+      dbo.taxiTrips 
+    WHERE 
+      [Payment Type] = 'Cash' 
+   );
