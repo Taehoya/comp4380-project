afd65871ec140b208202704f0ec49ee8d08aa7cb
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 23:19:39 -0500

    Update q9.sql

--- queries/q9.sql
+++ queries/q9.sql
@@ -3,18 +3,22 @@
 SELECT *
 FROM
   dbo.taxiTrips                                      
-WHERE
-  [Company] = 'City Service' 
-AND 
-  [Payment Type] = 'Mobile' 
-AND 
-  [Trip Seconds] > 1800) 
+WHERE 
+  (
+      [Company] = 'City Service' 
+  AND 
+      [Payment Type] = 'Mobile' 
+  AND 
+      [Trip Seconds] > 1800
+  ) 
 OR 
-  [Company] = 'Sun Taxi' 
-AND 
-  [Payment Type] = 'Mobile' 
-AND 
-  [Trip Seconds] > 1800);
+  (
+      [Company] = 'Sun Taxi' 
+  AND 
+      [Payment Type] = 'Mobile' 
+  AND 
+      [Trip Seconds] > 1800
+  );
   
   /* Optimized Query */
   
