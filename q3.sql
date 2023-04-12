48092319738ef3b3c0b850dc824b2921a4bff016
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:22:31 -0500

    Update q3.sql

--- q3.sql
+++ q3.sql
@@ -1,6 +1,9 @@
 /* Query 3 */
 
-SELECT * FROM dbo.taxiTrips 
-WHERE [Trip Start Timestamp] >= '2019-01-01 00:00:00.000' 
+SELECT * 
+FROM 
+  dbo.taxiTrips 
+WHERE 
+  [Trip Start Timestamp] >= '2019-01-01 00:00:00.000' 
 AND 
-[Trip End Timestamp] < '2019-12-31 23:59:59.999';
+  [Trip End Timestamp] < '2019-12-31 23:59:59.999';
