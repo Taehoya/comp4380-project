52fce7963cd8cdfc6ba01b4e32b24935912042ea
Tanisha <66926810+TTanisha@users.noreply.github.com>
2023-04-11 22:26:48 -0500

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -1,7 +1,23 @@
 /* Query 1 */
 
+/* Original Query */
 SELECT * 
 FROM 
   dbo.taxiTrips 
 WHERE 
   Tips = 0;
+
+/* Optimized Query */
+SELECT 
+  [Trip ID], [Taxi ID], [Tips] 
+FROM
+  dbo.taxiTrips
+WHERE 
+  [Trip ID] 
+IN (SELECT
+      [Trip ID]
+    FROM
+      dbo.taxiTrips
+    WHERE
+      [Tips] = 0
+   );
